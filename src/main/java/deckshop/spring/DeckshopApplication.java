00a47fd3f1325f163f5b96ae1fc48a17a02ec206
Ivorra-1996 <ivorrajosemaria@gmail.com>
2025-06-14 20:33:29 -0300

    creacion del proyecto

---
+++ src/main/java/deckshop/spring/DeckshopApplication.java
@@ -0,0 +1,13 @@
+package deckshop.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckshopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/deckshop/spring/domain/User.java
@@ -0,0 +1,9 @@
+package deckshop.spring.domain;
+
+
+// Atributos, getter, setters y constructor de usuarios. Se usan a nivel dominio
+public class User {
+
+    public User() {
+    }
+}
